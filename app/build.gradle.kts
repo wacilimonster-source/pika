ced1076921aa442fdff045c704be08adc6371868
wacilimonster <wacilimonster@users.noreply.github.com>
2026-08-14 06:02:46 +0800

    release 1.3.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.pika"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.3.5"
+        versionCode = 19
+        versionName = "1.3.6"
     }
 
     signingConfigs {
